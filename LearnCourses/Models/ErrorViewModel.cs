91f8fccb44f784392ffc6deb56d154ec216bdbe3
fasgetz <fasgetz@yandex.ru>
2021-09-17 16:09:18 +0300

    Добавьте файлы проекта.

---
+++ LearnCourses/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearnCourses.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
