3f9ba01d4c01e24c7c5a891752b7342c3b4f9280
fasgetz <fasgetz@yandex.ru>
2021-09-20 00:36:55 +0300

    Добавлена страницы политики в области обработки и защиты персональных данных и ссылки на нее

---
+++ LearnCourses/Controllers/OfertaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnCourses.Controllers
+{
+    public class OfertaController : Controller
+    {
+        public IActionResult PrivacyDataOferta()
+        {
+            return View();
+        }
+    }
+}
